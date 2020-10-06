dbca3ae699a6792d0fc3539918b5977d7495bf2f
Kris Gesling <kris.gesling@mycroft.ai>
2020-10-06 01:52:43 +0000

    first prototype - no voice command

--- __init__.py
+++ __init__.py
@@ -1,13 +1,19 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_handler
 
 
 class CommonVoice(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('voice.common.intent')
+    @intent_handler('record.samples.intent')
     def handle_voice_common(self, message):
-        self.speak_dialog('voice.common')
+        self.speak_dialog('common.voice.record')
+        self.gui.show_url('https://commonvoice.mozilla.org/en/speak')
+
+    @intent_handler('review.samples.intent')
+    def handle_review_samples(self, message):
+        self.speak_dialog('common.voice.review')
+        self.gui.show_url('https://commonvoice.mozilla.org/en/listen')
 
 
 def create_skill():
